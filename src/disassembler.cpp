cfbe25f22c4d57d3128d8ae2be6c8d0de8b1d28b
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2025-05-31 16:25:06 -0700

    Change indent width and add source information

--- src/disassembler.cpp
+++ src/disassembler.cpp
@@ -27,17 +27,36 @@ instruction instructions[] = {
 #include "quickjs-opcode.h"
 };
 
-void dump_bytecode(const JSFunctionBytecode * b, const int indent) {
+constexpr int INDENT_WIDTH = 2;
+
+void dump_bytecode(const JSFunctionBytecode * b, const bool strip, const int indent) {
     uint8_t * bytecode = b->byte_code_buf;
     const size_t bytecode_len = b->byte_code_len;
+
+    if (b->source_len > 0 && !strip) {
+        // Log source
+        std::cout << std::string(indent * INDENT_WIDTH, ' ') << "Source (Line " << b->line_num << "):" << std::endl
+            << std::string((indent + 1) * INDENT_WIDTH, ' ');
+
+        for (size_t i = 0; i < b->source_len; i++) {
+            const char character = b->source[i];
+            std::cout << character;
+            if (character == '\n') {
+                std::cout << std::string((indent + 1) * INDENT_WIDTH, ' ');
+            }
+        }
+
+        std::cout << std::endl;
+    }
 
     size_t i = 0;
 
     while (i < bytecode_len) {
-        instruction op = instructions[bytecode[i]];
-        size_t args = op.size - 1;
+        auto [name, size] = instructions[bytecode[i]];
+        const size_t args = size - 1;
 
-        std::cout << std::string(indent, ' ') << std::format("{:#04x}", bytecode[i]) << " (" << op.name << ") ";
+        std::cout << std::string(indent * INDENT_WIDTH, ' ') << std::format("{:#04x}", bytecode[i])
+            << " (" << name << ") ";
 
         for (size_t arg = 1; arg <= args; arg++) {
             std::cout << std::format("{:#04x}", bytecode[i+arg]) << " ";
@@ -45,30 +64,31 @@ void dump_bytecode(const JSFunctionBytecode * b, const int indent) {
 
         std::cout << std::endl;
 
-        if (op.name == "fclosure8") {
+        if (name == "fclosure8") {
             const uint8_t loc = bytecode[i+1];
             const auto * closure_bytecode = static_cast<JSFunctionBytecode *>(JS_VALUE_GET_PTR(b->cpool[loc]));
-            dump_bytecode(closure_bytecode, indent + 4);
-        } else if (op.name == "fclosure") {
+            dump_bytecode(closure_bytecode, strip, indent + 1);
+        } else if (name == "fclosure") {
             uint32_t loc;
             memcpy(&loc, bytecode + i + 1, sizeof(loc));
             const auto * closure_bytecode = static_cast<JSFunctionBytecode *>(JS_VALUE_GET_PTR(b->cpool[loc]));
-            dump_bytecode(closure_bytecode, indent + 4);
+            dump_bytecode(closure_bytecode, strip, indent + 1);
         }
 
-        i += op.size;
+        i += size;
     }
 }
 
-void dump_bytecode(const JSFunctionBytecode * b) {
-    dump_bytecode(b, 0);
+void dump_bytecode(const JSFunctionBytecode * b, const bool strip) {
+    dump_bytecode(b, strip, 0);
 }
 
 int main(const int argc, char * argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help,h", "print help message")
-        ("file,f", po::value<std::string>(), "input file containing code");
+        ("file,f", po::value<std::string>(), "input file containing code")
+        ("strip,s", "strip source information");
     po::variables_map vm;
     try {
         po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -116,7 +136,7 @@ int main(const int argc, char * argv[]) {
 
     auto * b = static_cast<JSFunctionBytecode *>(JS_VALUE_GET_PTR(obj));
 
-    dump_bytecode(b);
+    dump_bytecode(b, vm.contains("strip"));
 
     JS_FreeValue(ctx, obj);
     JS_FreeContext(ctx);
